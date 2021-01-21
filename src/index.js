4c5eebeb0275ca7066152f67cda1b7631b703179
critanaro <critanaro@gmail.com>
2021-01-20 23:55:56 -0500

    please

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import { Provider } from "rebass";
 import { injectGlobal } from "styled-components";
-import colors from "../styles/colors.json";
+import colors from "./styles/colors.json";
 
 // import necessary components
-import Head from "../components/head";
-import CenterImage from "../components/centerImage";
-import IntroText from "../components/introText";
-import CardNav from "../components/cardNav";
-import About from "../components/about";
-import Questions from "../components/faq";
-import Links from "../components/links";
-import Mlh from "../components/mlh";
-import Calendar from "../components/calendar";
-import Sponsors from "../components/sponsors";
-import Tracks from "../components/tracks";
-import Prizes from "../components/prizes"
+import Head from "./components/head";
+import CenterImage from "./components/centerImage";
+import IntroText from "./components/introText";
+import CardNav from "./components/cardNav";
+import About from "./components/about";
+import Questions from "./components/faq";
+import Links from "./components/links";
+import Mlh from "./components/mlh";
+import Calendar from "./components/calendar";
+import Sponsors from "./components/sponsors";
+import Tracks from "./components/tracks";
+import Prizes from "./components/prizes"
 
 // import necessary contents
-import { title as aboutTitle, lines as lines} from "../content/about";
-import { cards } from "../content/cardNav";
-import { title as questionsTitle, questions } from "../content/faq";
-import { intro, bullets } from "../content/introText";
-import { links } from "../content/links";
-import {billLines, billTitle} from "../content/aboutBill";
+import { title as aboutTitle, lines as lines} from "./content/about";
+import { cards } from "./content/cardNav";
+import { title as questionsTitle, questions } from "./content/faq";
+import { intro, bullets } from "./content/introText";
+import { links } from "./content/links";
+import {billLines, billTitle} from "./content/aboutBill";
 
 // Set global body styling.
 injectGlobal`
@@ -33,7 +33,7 @@ injectGlobal`
 // Variables for <head>
 let title = "RSHack";
 let url = "https://hack.rice.edu";
-let ogImage = "../static/blueH.png";
+let ogImage = "./static/blueH.png";
 let description = "The inaugural Rice SASE Hackathon!";
 
 /**
